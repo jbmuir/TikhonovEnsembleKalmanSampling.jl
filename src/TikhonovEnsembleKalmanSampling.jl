7adbeecffd5b7ded6bb26ff4fb1a281741238fe5
Jack B. Muir <jmuir@caltech.edu>
2020-10-29 13:28:49 -0700

    Split functions into update & wrapper

--- src/TikhonovEnsembleKalmanSampling.jl
+++ src/TikhonovEnsembleKalmanSampling.jl
@@ -7,6 +7,7 @@ using Optim
 
 include("sampling.jl")
 
+export teks_update, whteks_update
 export teks, whteks
 
 end # module
--- src/sampling.jl
+++ src/sampling.jl
@@ -1,3 +1,36 @@
+function teks_update(u::Array{T,2}, y, Γ⁻¹, C₀⁻¹, G; 
+    parallel=false, 
+    h₀=1, 
+    iδ=100, 
+    λ=1, 
+    ϵ=convert(T, 1e-10)) where T
+
+    J = size(u, 2)
+    #calculate i-step sample covariance in parameter space
+    ū = mean(u, dims=2)
+    Cu = Symmetric((u .- ū)*(u .- ū)')/J
+    Cuc = cholesky(Cu + ϵ*I) 
+    # forward map and differencing operator
+    if parallel
+        g = reduce(hcat, pmap(G, eachcol(u)))
+    else
+        g = reduce(hcat, map(G, eachcol(u)))
+    end
+    ḡ = mean(g, dims=2)
+    D = (g .- y)' * (Γ⁻¹ * (g .- ḡ))
+    #Regularization/prior, adaptive stepsize, gradient step
+    ∇R = Cu*C₀⁻¹
+    hₙ = h₀ / (norm(D) + 1/iδ)
+    Du = u .- ((hₙ/J)*D*u')'
+    #Implicit solve of partial step
+    I∇R = cholesky(I + hₙ*∇R)
+    u = I∇R \ Du
+    # perturb parameters according to i-step sample covariance
+    Wu = randn(T, size(u))
+    u .+= sqrt(2*hₙ)*Cuc.U*Wu
+    return (u, hₙ)
+end
+
 function teks(u::Array{T,2}, y, Γ⁻¹, C₀⁻¹, G, n_steps; 
               parallel=false, 
               h₀=1, 
@@ -9,30 +42,13 @@ function teks(u::Array{T,2}, y, Γ⁻¹, C₀⁻¹, G, n_steps;
         uchain = Array{T,2}[]
     end
     hₙchain = T[]
-    J = size(u, 2)
     for i = 1:n_steps
-        #calculate i-step sample covariance in parameter space
-        ū = mean(u, dims=2)
-        Cu = Symmetric((u .- ū)*(u .- ū)')/J
-        Cuc = cholesky(Cu + ϵ*I) 
-        # forward map and differencing operator
-        if parallel
-            g = reduce(hcat, pmap(G, eachcol(u)))
-        else
-            g = reduce(hcat, map(G, eachcol(u)))
-        end
-        ḡ = mean(g, dims=2)
-        D = (g .- y)' * (Γ⁻¹ * (g .- ḡ))
-        #Regularization/prior, adaptive stepsize, gradient step
-        ∇R = Cu*C₀⁻¹
-        hₙ = h₀ / (norm(D) + 1/iδ)
-        Du = u .- ((hₙ/J)*D*u')'
-        #Implicit solve of partial step
-        I∇R = cholesky(I + hₙ*∇R)
-        u = I∇R \ Du
-        # perturb parameters according to i-step sample covariance
-        Wu = randn(T, size(u))
-        u .+= sqrt(2*hₙ)*Cuc.U*Wu
+        u, hₙ = teks_update(u, y, Γ⁻¹, C₀⁻¹, G; 
+                            parallel=parallel, 
+                            h₀=h₀, 
+                            iδ=iδ, 
+                            λ=λ, 
+                            ϵ=ϵ)
         if savechain
             push!(uchain, u)
         end
@@ -52,6 +68,54 @@ function implicit_hyper_solve(θΔθ, Cθ, hₙ, ∇nlpθ_fun; solver=NelderMead
     return Optim.minimizer(res)
 end
 
+function whteks_update(ξ::Array{T,2}, θ::Array{T,2}, y, Γ⁻¹, GT, ∇nlpθ_fun; 
+                        parallel=false, 
+                        h₀=1, 
+                        iδ=100, 
+                        λ=1, 
+                        ϵ=convert(T, 1e-10), 
+                        hyper_solver=NelderMead()) where T
+    J = size(ξ, 2)
+    #calculate i-step sample covariance in parameter space & update parameters
+    ξ̄ = mean(ξ, dims=2)
+    θ̄ = mean(θ, dims=2)
+    Cξ = Symmetric((ξ .- ξ̄)*(ξ .- ξ̄)')/J
+    Cθ = Symmetric((θ .- θ̄)*(θ .- θ̄)')/J
+    Cξc = cholesky(Cξ + ϵ*I)
+    Cθc = cholesky(Cθ + ϵ*I) 
+    # forward map and differencing operator
+    if parallel
+        g = reduce(hcat, pmap(GT, zip(eachcol(ξ), eachcol(θ))))
+    else
+        g = reduce(hcat, map(GT, zip(eachcol(ξ), eachcol(θ))))
+    end
+    ḡ = mean(g, dims=2)
+    D = (g .- y)' * (Γ⁻¹*(g .- ḡ))
+    #Regularization / prior and adaptive stepsize, gradient step
+    ∇Rξ = Cξ
+    hₙ = h₀ / (norm(D) + 1/iδ)
+    Dξ = ξ .- ((hₙ/J)*D*ξ')'
+    Dθ = θ .- ((hₙ/J)*D*θ')'
+    #Implicit solve of partial step
+    I∇Rξ = cholesky(I + hₙ*∇Rξ)
+    ξ = I∇Rξ \ Dξ
+    if parallel
+        θ = reduce(hcat, 
+                    pmap(θDθ->implicit_hyper_solve(θDθ, Cθ, hₙ, ∇nlpθ_fun, solver=hyper_solver),  
+                    zip(eachcol(θ), eachcol(Dθ))))    
+    else
+        θ = reduce(hcat, 
+                    map(θDθ->implicit_hyper_solve(θDθ, Cθ, hₙ, ∇nlpθ_fun, solver=hyper_solver),  
+                    zip(eachcol(θ), eachcol(Dθ))))        
+    end        
+    # perturb parameters according to i-step sample covariance
+    Wξ = randn(T, size(ξ))
+    Wθ = randn(T, size(θ))
+    ξ .+= sqrt(2*hₙ)*Cξc.U*Wξ
+    θ .+= sqrt(2*hₙ)*Cθc.U*Wθ
+    return (ξ, θ, hₙ)
+end
+
 function whteks(ξ::Array{T,2}, θ::Array{T,2}, y, Γ⁻¹, GT, ∇nlpθ_fun, n_steps; 
                 parallel=false, 
                 h₀=1, 
@@ -68,43 +132,13 @@ function whteks(ξ::Array{T,2}, θ::Array{T,2}, y, Γ⁻¹, GT, ∇nlpθ_fun, n_
     hₙchain = T[]
     J = size(ξ, 2)
     for i = 1:n_steps
-        #calculate i-step sample covariance in parameter space & update parameters
-        ξ̄ = mean(ξ, dims=2)
-        θ̄ = mean(θ, dims=2)
-        Cξ = Symmetric((ξ .- ξ̄)*(ξ .- ξ̄)')/J
-        Cθ = Symmetric((θ .- θ̄)*(θ .- θ̄)')/J
-        Cξc = cholesky(Cξ + ϵ*I)
-        Cθc = cholesky(Cθ + ϵ*I) 
-        # forward map and differencing operator
-        if parallel
-            g = reduce(hcat, pmap(GT, zip(eachcol(ξ), eachcol(θ))))
-        else
-            g = reduce(hcat, map(GT, zip(eachcol(ξ), eachcol(θ))))
-        end
-        ḡ = mean(g, dims=2)
-        D = (g .- y)' * (Γ⁻¹*(g .- ḡ))
-        #Regularization / prior and adaptive stepsize, gradient step
-        ∇Rξ = Cξ
-        hₙ = h₀ / (norm(D) + 1/iδ)
-        Dξ = ξ .- ((hₙ/J)*D*ξ')'
-        Dθ = θ .- ((hₙ/J)*D*θ')'
-        #Implicit solve of partial step
-        I∇Rξ = cholesky(I + hₙ*∇Rξ)
-        ξ = I∇Rξ \ Dξ
-        if parallel
-            θ = reduce(hcat, 
-                       pmap(θDθ->implicit_hyper_solve(θDθ, Cθ, hₙ, ∇nlpθ_fun, solver=hyper_solver),  
-                       zip(eachcol(θ), eachcol(Dθ))))    
-        else
-            θ = reduce(hcat, 
-                       map(θDθ->implicit_hyper_solve(θDθ, Cθ, hₙ, ∇nlpθ_fun, solver=hyper_solver),  
-                       zip(eachcol(θ), eachcol(Dθ))))        
-        end        
-        # perturb parameters according to i-step sample covariance
-        Wξ = randn(T, size(ξ))
-        Wθ = randn(T, size(θ))
-        ξ .+= sqrt(2*hₙ)*Cξc.U*Wξ
-        θ .+= sqrt(2*hₙ)*Cθc.U*Wθ
+        ξ, θ, hₙ = whteks_update(ξ, θ, y, Γ⁻¹, GT, ∇nlpθ_fun; 
+                                parallel=parallel, 
+                                h₀=h₀, 
+                                iδ=iδ, 
+                                λ=λ, 
+                                ϵ=ϵ, 
+                                hyper_solver=hyper_solver)
         if savechain
             push!(ξchain, ξ)
             push!(θchain, θ)
